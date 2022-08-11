7c25214c26ef524891f3b90cf66368759149b5fb
Sardote <vitoleg@gmail.com>
2022-08-11 14:23:02 +0300

    part1 ready

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
 import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
@@ -24,9 +26,67 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   // TODO(task): Implement Auth screen.
+  final loginField = TextEditingController(text: 'sardote');
+  final passwordField = TextEditingController(text: '2Sc7rvXOGMHg');
+  late TokenDto token;
+  
+
+  void _login() async {
+    final login = loginField.text;
+    final password = passwordField.text;
+    try {
+      token =
+          await widget.authRepository.signIn(login: login, password: password);
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', token.token);
+      _pushToChat(context, token);
+    } on Exception catch (e) {
+      _showSnakBar(e.toString());
+    }
+  }
+
+  void _showSnakBar(final String message){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('$message')));
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: loginField,
+              
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+                labelText: 'Login',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: passwordField,
+              obscureText: true,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.lock_outlined),
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: _login,
+              child: const Text('Далее'),
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 
   void _pushToChat(BuildContext context, TokenDto token) {
